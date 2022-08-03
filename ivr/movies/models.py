0ff3843aace53b4cdbdde9b42c580a107129eb63
Gobuub <botica1982@msn.com>
2022-08-03 17:55:14 +0200

    Initial commit

---
+++ ivr/movies/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# First create three models, Theater and Movies include digits attr for choosing the movie on IVR Menu
+
+
+class Theater(models.Model):
+    class Meta:
+        verbose_name = 'Theater'
+        verbose_name_plural = 'Theaters'
+
+    name = models.CharField(max_length=50)
+    address = models.TextField()
+    digits = models.PositiveSmallIntegerField(unique=True)
+
+
+class Movie(models.Model):
+    class Meta:
+        verbose_name = 'Movie'
+        verbose_name_plural = 'Movies'
+
+    title = models.CharField(max_length=50)
+    digits = models.PositiveSmallIntegerField(unique=True)
+
+
+class Show(models.Model):
+    class Meta:
+        verbose_name = 'Show'
+        verbose_name_plural = 'Shows'
+
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    theater = models.ForeignKey(Theater, on_delete=models.CASCADE)
+    start_at = models.DateTimeField()
---
+++ ivr/movies/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from twilio.twiml.voice_response import VoiceResponse
+# Create your views here.
+
+# First we will create a new view that accepts an HttpRequest object and returns an HttpResponse
+
+
+@csrf_exempt
+# we use the csrf decorator to disabled Django´s CSRF protection, without this we can't use twilio on our website
+def answer(request: HttpRequest) -> HttpResponse:
+    # with this create an instance of VoiceResponse
+    vr = VoiceResponse()
+    # then use the 'say' function of the VoiceResponse object to create a TwiML markup to greet the user 'Hello'
+    # using text-to-speech
+    vr.say('Hello Yonatan')
+    return HttpResponse(str(vr), content_type='text/xml')
